e22b42fd26f13007e14fdb7a886d4f5c5293c586
Greg <gregoryjamesthomas@gmail.com>
2015-04-03 02:27:46 +0000

    Adding pi project files to repo

---
+++ send_sms.py
@@ -0,0 +1,13 @@
+
+from twilio.rest import TwilioRestClient
+ 
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = "AC8db72cd36749487872cfd5fcc0725c37"
+auth_token  = "0f46ec208b28c4d06a9a1c7dae4cb080"
+client = TwilioRestClient(account_sid, auth_token)
+ 
+message = client.messages.create(body="Apartment door has been opened",
+    to="+16466732481",    # My phone number
+    from_="+12018856036") # My Twilio number
+print message.sid
+
---
+++ switch.py
@@ -0,0 +1,17 @@
+import time
+import RPi.GPIO as io
+io.setmode(io.BCM)
+
+pir_pin = 18
+door_pin = 23
+
+io.setup(pir_pin, io.IN)         # activate input
+io.setup(door_pin, io.IN, pull_up_down=io.PUD_UP)  # activate input with PullUp
+
+while True:
+    if io.input(pir_pin):
+        print("PIR ALARM!")
+    if io.input(door_pin):
+        execfile("send_sms.py")
+    time.sleep(0.5)
+
